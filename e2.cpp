b1b1f0fcb0036a64cdc57ff3d644afe2084d3066
JoeCharbel1 <joecharbel.dergaspard@net.usj.edu.lb>
2025-10-16 21:45:28 +0300

    e2, error messsage bad solution

--- e2.cpp
+++ e2.cpp
@@ -8,6 +8,9 @@ int main(){
     do{
     cout<<"Enter a positive integer less than 70: "<<endl;
     cin>>n;
+    if (n<=0){
+        cout << "Your number is less than or equal to 0!"<<endl;
+    }
     }while (n<=0 || n>=70);
     for(int i=10; i>=0; i--)
     {
