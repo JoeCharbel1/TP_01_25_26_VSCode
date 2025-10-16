e25eb1e887f7b71397f41eeba11aafb6dbdd9e75
JoeCharbel1 <joecharbel.dergaspard@net.usj.edu.lb>
2025-10-16 21:41:47 +0300

    e2, an integer that cannot be less than zero

--- e2.cpp
+++ e2.cpp
@@ -5,9 +5,12 @@ using namespace std;
 using namespace std;
 int main(){
     int n;
+    do{
     cout<<"Enter a positive integer:"<<endl;
     cin>>n;
-    for(int i=10; i>=0; i--){
+    }while (n<=0);
+    for(int i=10; i>=0; i--)
+    {
     cout<<n<<"x"<<i<<"="<<n*i<<endl;
     }
     return 0;
